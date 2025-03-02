6ef814234d540ba9a7ad800edf764959ff6b3c5b
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-02 22:53:34 +0000

    almost complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,47 @@
 public class PracticeProblem {
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
 
+	public static int calculate(int num1, int num2, char op) {
+
+		int result = 0;
+
+		switch (op) {
+			case '+':
+				result = num1+num2;
+				break;
+			case '-':
+				result = num1-num2;
+				break;
+			case '*':
+				result = num1*num2;
+				break;
+			case '/':
+				result = num1/num2;
+				break;
+			case '%':
+				result = num1%num2;
+				break;
+			case '^':
+				result = (int)Math.pow(num1, num2);
+				break;
+		}
+
+		return result;
+
+	}
+
+	final static int MAX_VALUE = 10;
+	public static boolean totalWordsChecker(String word) {
+
+		boolean isMax = false;
+		String[] wordCount = word.split("[ ,./;({<-]");
+
+		if (wordCount.length <= MAX_VALUE) {
+			isMax = true;
+		}
+
+		return isMax;
+		
 	}
 
 }
